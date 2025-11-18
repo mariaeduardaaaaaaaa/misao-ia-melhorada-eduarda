85884eca966debbe361b2a5bd68f286dfceb283a
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-11-18 11:27:03 -0300

    18/11

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { aletorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
